54b2d83bc98c75698fd1dbd6f6a0e480497881dc
SMN-web <nafil.8895.s@gmail.com>
2025-08-19 19:00:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
 import { initSignup } from './signup.js';
-import { initLogin } from './login.js';
 
 const appDiv = document.getElementById('app');
-const auth = window.firebaseAuth;
 
 function router() {
   const hash = window.location.hash || '#signup';
+  console.log("Routing to:", hash);
 
   if (hash === '#signup') {
-    initSignup(appDiv, auth);
-  } else if (hash === '#login') {
-    initLogin(appDiv, auth);
+    initSignup(appDiv);
   } else {
     appDiv.innerHTML = '<h2>Page not found</h2>';
   }
 }
-
-window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
