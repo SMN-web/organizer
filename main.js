d0722267572bc9073b360439e3dbf276abc3c664
SMN-web <nafil.8895.s@gmail.com>
2025-08-19 19:51:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 import { initSignup } from './signup.js';
-import { initLogin } from './login.js';
+// You will add login import later for example:
+// import { initLogin } from './login.js';
 
 const appDiv = document.getElementById('app');
 const auth = window.firebaseAuth;
 
 function router() {
   const hash = window.location.hash || '#signup';
+  console.log("Router hash:", hash);
 
   if (hash === '#signup') {
     initSignup(appDiv, auth);
-  } else if (hash === '#login') {
-    initLogin(appDiv, auth);
-  } else {
+  } 
+  // Add this when login.js ready:
+  // else if (hash === '#login') {
+  //   initLogin(appDiv, auth);
+  // } 
+  else {
     appDiv.innerHTML = '<h2>Page not found</h2>';
   }
 }
