b2816b7ee6550b8bbf57e692a80bd273be9a7252
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 10:26:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-import { initSignup } from './signup.js';
-import { initTerms } from './terms.js';
-
 const appDiv = document.getElementById('app');
+
+function showSignup(container) {
+  container.innerHTML = `
+    <h2>Sign Up Page</h2>
+    <button id="gotoTerms">Go to Terms</button>
+  `;
+  // Simple navigation
+  document.getElementById('gotoTerms').onclick = () => {
+    window.location.hash = '#terms';
+  };
+}
+
+function showTerms(container) {
+  container.innerHTML = `
+    <h2>Terms Page</h2>
+    <button id="backSignup">Back to Sign Up</button>
+  `;
+  document.getElementById('backSignup').onclick = () => {
+    window.location.hash = '#signup';
+  };
+}
 
 function router() {
   const hash = window.location.hash || '#signup';
   if (hash === '#signup') {
-    initSignup(appDiv);
+    showSignup(appDiv);
   } else if (hash === '#terms') {
-    initTerms(appDiv);
+    showTerms(appDiv);
   } else {
     appDiv.innerHTML = "<h2>Page not found</h2>";
   }
