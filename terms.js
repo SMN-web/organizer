f9b5e5b07df8202b89f349ddb3a78bd62598b2b7
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 08:31:44 +0300

    Create terms.js

---
+++ terms.js
@@ -0,0 +1,7 @@
+export function initTerms(container) {
+  container.innerHTML = `
+    <h2>Terms and Conditions</h2>
+    <p>This is a sample Terms and Conditions page for your app.</p>
+    <p>Please review all terms carefully before accepting.</p>
+  `;
+}
