10c8fcd8c7809373e8e9faa6073358c7424bf1ee
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 08:05:16 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,3 +1,11 @@
 export function initSignup(container, auth) {
-  container.innerHTML += "<h2>Signup.js loaded!</h2>";
+  container.innerHTML += `
+    <h2>Sign Up</h2>
+    <form id="signupForm" novalidate>
+      <input type="email" id="email" placeholder="Email" required />
+      <input type="password" id="password" placeholder="Password" required />
+      <button type="submit">Sign Up</button>
+      <span id="formError"></span>
+    </form>
+  `;
 }
