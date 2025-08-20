b1329c596f2d0751cbf64499cb8d2f91a1493ef4
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 08:01:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
+import { initSignup } from './signup.js';
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = '<h2>Main.js loaded!</h2>';
+
+initSignup(appDiv, window.firebaseAuth);
