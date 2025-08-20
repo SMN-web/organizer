888e852caf4150412f94c65322838b6f416479d9
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 17:30:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 import { showSignup } from './signup.js';
+import { showLogin } from './login.js';
 import { showTerms } from './terms.js';
-import { showResendVerification } from './resendVerification.js'; // <-- import your new module
+import { showResendVerification } from './resendVerification.js';
+import { showUserPanel } from './userPanel.js';     // You can expand later
+import { showAdminPanel } from './adminPanel.js';   // You can expand later
+import { showModeratorPanel } from './moderatorPanel.js'; // You can expand later
 
 const appDiv = document.getElementById('app');
 
 function router() {
   try {
-    const hash = window.location.hash || '#signup';
+    const hash = window.location.hash || '#login';
     if (hash === '#signup') {
       showSignup(appDiv);
+    } else if (hash === '#login') {
+      showLogin(appDiv);
     } else if (hash === '#terms') {
       showTerms(appDiv);
     } else if (hash === '#resend') {
       showResendVerification(appDiv);
+    } else if (hash === '#user') {
+      showUserPanel(appDiv);
+    } else if (hash === '#admin') {
+      showAdminPanel(appDiv);
+    } else if (hash === '#moderator') {
+      showModeratorPanel(appDiv);
     } else {
-      window.location.hash = '#signup';
+      window.location.hash = '#login';
     }
   } catch (err) {
     appDiv.innerHTML = `<pre style="color:red">Router error: ${err.message}</pre>`;
