7813c3905a306176cf0286b94694feefb280ae3c
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 08:19:58 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,11 +1,38 @@
 export function initSignup(container, auth) {
-  container.innerHTML += `
+  container.innerHTML = `
     <h2>Sign Up</h2>
     <form id="signupForm" novalidate>
-      <input type="email" id="email" placeholder="Email" required />
-      <input type="password" id="password" placeholder="Password" required />
+      <label>
+        Username:<br/>
+        <input type="text" id="username" required autocomplete="off" placeholder="Username">
+      </label><br/>
+
+      <label>
+        Full Name:<br/>
+        <input type="text" id="name" required autocomplete="off" placeholder="Full name">
+      </label><br/>
+
+      <label>
+        Email:<br/>
+        <input type="email" id="email" required autocomplete="off" placeholder="Email">
+      </label><br/>
+
+      <label>
+        Password:<br/>
+        <input type="password" id="password" required autocomplete="off" placeholder="Password">
+      </label><br/>
+
+      <label>
+        Confirm Password:<br/>
+        <input type="password" id="confirmPassword" required autocomplete="off" placeholder="Confirm password">
+      </label><br/>
+
+      <label>
+        <input type="checkbox" id="terms" required>
+        I accept the <a href="#" target="_blank">terms and conditions</a>
+      </label><br/><br/>
+
       <button type="submit">Sign Up</button>
-      <span id="formError"></span>
     </form>
-  `; 
+  `;
 }
