55ca426d561a89917248847673318a9cdb1f8735
SMN-web <nafil.8895.s@gmail.com>
2025-08-20 16:25:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import { showSignup } from './signup.js';
 import { showTerms } from './terms.js';
+import { showResendVerification } from './resendVerification.js'; // <-- import your new module
 
 const appDiv = document.getElementById('app');
 
 function router() {
   try {
-    // Default to #signup if hash missing or unknown
     const hash = window.location.hash || '#signup';
     if (hash === '#signup') {
       showSignup(appDiv);
     } else if (hash === '#terms') {
       showTerms(appDiv);
+    } else if (hash === '#resend') {
+      showResendVerification(appDiv);
     } else {
       window.location.hash = '#signup';
     }
@@ -21,4 +23,4 @@ function router() {
 
 window.addEventListener('hashchange', router);
 window.addEventListener('load', router);
-router(); // initial render
+router();
