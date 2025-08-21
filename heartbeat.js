2499336f7b9ccda9cc3c41716ed8911d373d90d2
SMN-web <nafil.8895.s@gmail.com>
2025-08-21 17:39:58 +0300

    Update heartbeat.js

--- heartbeat.js
+++ heartbeat.js
@@ -1,6 +1,6 @@
 export function sendHeartbeat(token) {
   if (!token) return;
-  fetch("https://heartbeat.nafil-8895-s.workers.dev/api/heartbeat", {
+  fetch("https://ad-api.nafil-8895-s.workers.dev/api/heartbeat", {
     method: "POST",
     headers: { "Authorization": "Bearer " + token }
   });
