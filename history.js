8d15957997b4e46ccf7b90e1225d7e962b97e99f
SMN-web <nafil.8895.s@gmail.com>
2025-09-04 14:22:39 +0300

    Update history.js

--- history.js
+++ history.js
@@ -1,5 +1,4 @@
 export async function showHistoryPanel(container, user) {
-  container.innerHTML = `<div>Loading transaction history...</div>`;
-  // Fetch full ledger/history using user.firebaseUser.getIdToken()
-  // Render all finalized payment and transfer bubbles with expanded details, status dates, etc.
+  container.innerHTML = `<div style="color:#888;">(Transaction history coming soon.)</div>`;
+  // TODO: Implement unified fetch and display of history
 }
