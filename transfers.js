cbc88713222e49d0e050d6096d6956a78afae30b
SMN-web <nafil.8895.s@gmail.com>
2025-09-04 15:58:54 +0300

    Create transfers.js

---
+++ transfers.js
@@ -0,0 +1,5 @@
+export function showTransfersPanel(container, user) {
+  container.innerHTML = `<div style="padding:2em; text-align:center; color:#156;">
+    <b>Transfers</b> panel active (test).
+  </div>`;
+}
