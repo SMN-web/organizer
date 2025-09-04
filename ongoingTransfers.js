8f74504fcb032a2238a9d6b40bdc64b7be7a9da1
SMN-web <nafil.8895.s@gmail.com>
2025-09-04 17:06:02 +0300

    Update ongoingTransfers.js

--- ongoingTransfers.js
+++ ongoingTransfers.js
@@ -1,9 +1,8 @@
 export function showOngoingTransfersPanel(container, user) {
   container.innerHTML = `
-    <div style="padding:2em;text-align:center;">
-      <b>Ongoing Transfers</b><br>
-      <span style="color:#888;">There are no ongoing transfers at the moment.</span>
-      <!-- Add your dynamic ongoing transfer list/UI here -->
+    <div>
+      <b>Ongoing Transfers</b>
+      <div>There are no ongoing transfers at the moment.</div>
     </div>
   `;
 }
