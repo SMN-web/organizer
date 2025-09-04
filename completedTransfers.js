07731a72cb17b99db74d4121531c89f34d0ec427
SMN-web <nafil.8895.s@gmail.com>
2025-09-04 17:06:24 +0300

    Update completedTransfers.js

--- completedTransfers.js
+++ completedTransfers.js
@@ -1,9 +1,8 @@
 export function showCompletedTransfersPanel(container, user) {
   container.innerHTML = `
-    <div style="padding:2em;text-align:center;">
-      <b>Completed Transfers</b><br>
-      <span style="color:#888;">No transfers have been completed yet.</span>
-      <!-- Add your dynamic completed transfer list/UI here -->
+    <div>
+      <b>Completed Transfers</b>
+      <div>No transfers have been completed yet.</div>
     </div>
   `;
 }
