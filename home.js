e065fda2ae1e82476ae44ce283f629790393d652
SMN-web <nafil.8895.s@gmail.com>
2025-09-04 16:30:37 +0300

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-export function renderHome(container) {
-  container.innerHTML = `
-    <h1>Minimal SPA Module Test</h1>
-    <p>Your device and browser fully support JavaScript ES modules!</p>
-    <p>Try changing the URL hash to #home</p>
-  `;
-  console.log('Home view rendered');
-}
