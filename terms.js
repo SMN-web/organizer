126386eb7929bafdc3d869d03183ceecbe036f56
SMN-web <nafil.8895.s@gmail.com>
2025-09-15 13:46:44 +0300

    Update terms.js

--- terms.js
+++ terms.js
@@ -1,7 +1,7 @@
 export function showTerms(container) {
   container.innerHTML = `
     <h2>Terms and Conditions</h2>
-    <p>This is a sample Terms and Conditions screen.</p>
+    <p>All activities are solely depends on the my own interest. Terms and conditions will change without prior notice.</p>
     <button id="backSignup">Back to Sign Up</button>
   `;
   document.getElementById('backSignup').onclick = () => {
